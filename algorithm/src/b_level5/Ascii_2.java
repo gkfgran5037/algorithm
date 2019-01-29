0e76920ee9a20d451e23b4c8ff4ad2b083056aed
gkfgran5037 <gkfgran5037@naver.com>
2019-01-29 22:38:21 +0900

    Create Ascii 1~2
    백준알고리즘11654번 - 아스키코드
    2가지 버전 작성

---
+++ algorithm/src/b_level5/Ascii_2.java
@@ -0,0 +1,20 @@
+package b_level5;
+
+import java.io.IOException;
+
+/* 백준알고리즘 11654번 - 아스키코드
+ * 
+ * 분제
+ *  - 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+ * 
+ * 입력
+ *  - 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+ *    
+ * 출력
+ *  - 입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+ */
+public class Ascii_2 {
+	public static void main(String[] args) throws IOException {
+		System.out.println(System.in.read());
+	}
+}
